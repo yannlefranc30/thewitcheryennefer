809a0bbd96c25a5776b2397ed453f643eb94e5ed
yann <yannlefranc330@gmail.com>
2023-05-03 19:39:22 +0200

    the wotcher bout de code avec un integer

---
+++ src/thewitcher.java
@@ -0,0 +1,12 @@
+public class thewitcher {
+    public static void main( String [] args ) {
+
+        Integer value = 123;                // Valeur de l'integer
+        System.out.println( value );        // Affiche 123
+
+        // Il est même possible d'écrire
+        value += 9;
+        System.out.println( value );        // je passe à 132;
+
+    }
+}
